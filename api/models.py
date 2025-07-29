fea543aeb31b6204ed8290b71b27580ec80fb96f
(670108) ณรงค์กรณ์ อัจฉริยญาติ <hardman16p@gmail.com>
2025-07-29 21:03:40 +0700

    Implement new FastAPI endpoint

--- api/models.py
+++ api/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Integer, Date
-from app.database import Base
+from .database import Base
 
 # ── User Model ───────────────────────────────────────
 class User(Base):
