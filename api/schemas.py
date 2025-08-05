b2e2cde5228cf3e8064d4822480784db2bd35768
(670108) ณรงค์กรณ์ อัจฉริยญาติ <hardman16p@gmail.com>
2025-08-05 21:06:52 +0700

    Implement new FastAPI endpoint

--- api/schemas.py
+++ api/schemas.py
@@ -57,25 +57,30 @@ from pydantic import BaseModel
 from typing import Optional
 from datetime import date, datetime
 
+
 class JobUpdateSchema(BaseModel):
-    date_plan: Optional[date] = None
-    h_plate: Optional[str] = None
-    t_plate: Optional[str] = None
+    load_id: str                      # required
+    date_plan: date                   # required
+    h_plate: str                      # required
+    t_plate: str                      # required
+    driver_name: str                  # required
+    status: str                       # required
+    locat_recive: str                 # required
+    date_recive: date                 # required
+    locat_deliver: str                # required
+    date_deliver: date                # required
+    pallet_type: str                  # required
+    pallet_plan: int                  # required
+    created_by: str                   # required
+    created_at: date                  # required
+
+    # ที่เหลือเป็น optional
     fuel_type: Optional[str] = None
     height: Optional[str] = None
     weight: Optional[str] = None
-    driver_name: Optional[str] = None
     phone: Optional[str] = None
-    status: Optional[str] = None
     remark: Optional[str] = None
-    locat_recive: Optional[str] = None
-    date_recive: Optional[date] = None
-    locat_deliver: Optional[str] = None
-    date_deliver: Optional[date] = None
-    pallet_type: Optional[str] = None
-    pallet_plan: Optional[int] = None
     unload_cost: Optional[str] = None
-    # ไม่ต้องมี load_id ใน update schema
 
     class Config:
         orm_mode = True
