6f76a5a7c1abb2b4bc255a7c1cb6baef6985835d
(670108) ณรงค์กรณ์ อัจฉริยญาติ <hardman16p@gmail.com>
2025-10-11 11:55:31 +0700

    gps data and no timezone

--- api/index.py
+++ api/index.py
@@ -330,7 +330,7 @@ def get_jobs(
             "gps_id": vehicle.gps_id if vehicle else None,
             "current_latlng": vehicle.current_latlng if vehicle else None,
             "gps_updated_at": (
-                vehicle.gps_updated_at.astimezone(ZoneInfo("Asia/Bangkok")).isoformat()
+                vehicle.gps_updated_at.isoformat()
                 if vehicle and vehicle.gps_updated_at else None
             ),
         }
