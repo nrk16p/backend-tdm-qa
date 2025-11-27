a33fff9b75a6043ad6ccefd4d7242ce910badcf7
(670108) ณรงค์กรณ์ อัจฉริยญาติ <hardman16p@gmail.com>
2025-11-27 13:24:26 +0700

    Add new  status update for route

--- api/index.py
+++ api/index.py
@@ -329,6 +329,8 @@ def get_jobs(
             "gps_vendor": vehicle.gps_vendor if vehicle else None,
             "gps_id": vehicle.gps_id if vehicle else None,
             "current_latlng": vehicle.current_latlng if vehicle else None,
+            "status": vehicle.status if vehicle else None,
+
             "gps_updated_at": (
                 vehicle.gps_updated_at.isoformat()
                 if vehicle and vehicle.gps_updated_at else None
